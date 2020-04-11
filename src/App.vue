6010cd2d941dd2416bdb2071d4e6989c9be41921
didemertens <d.g.j.mertens@gmail.com>
2020-04-11 18:48:38 +0100

    add get request to Google Books API

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,10 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 import SearchBar from './components/SearchBar'
+const booksKey = process.env.VUE_APP_BOOKS_KEY
 
 export default {
   name: 'App',
@@ -14,6 +17,9 @@ export default {
   },
   methods: {
     onTermChange(searchTerm) {
+      axios.get(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&key=${booksKey}`)
+      .then(response => console.log(response))
+      console.log(booksKey)
       console.log(searchTerm)
     }
   }
