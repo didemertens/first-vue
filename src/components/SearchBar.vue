96b7bc40db23ba5aeb826dcebedf6fb3d2e54bd4
didemertens <d.g.j.mertens@gmail.com>
2020-04-12 12:31:49 +0100

    render search again when clicking on radio buttons

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -2,10 +2,14 @@
   <div>
     <h3>Find books</h3>
     <input class="searchbar" @input="onInput" type="text" />
-    <input @click="onRadioClick" type="radio" id="title" value="title" name="search-filter" checked />
-    <label for="title">Title</label>
-    <input @click="onRadioClick" type="radio" id="author" value="author" name="search-filter" />
-    <label for="title">Author</label>
+    <div class="form-check form-check-inline">
+      <input class="form-check-input" @click="onRadioClick" type="radio" id="title" value="title" name="search-filter" checked />
+      <label class="form-check-label" for="title">Title</label>
+    </div>
+    <div class="form-check form-check-inline">
+      <input class="form-check-input" @click="onRadioClick" type="radio" id="author" value="author" name="search-filter" />
+      <label class="form-check-label" for="title">Author</label>
+    </div>
   </div>
 </template>
 
@@ -14,15 +18,18 @@ export default {
   name: "SearchBar",
   data () {
     return {
+      searchTerm: null,
       searchFilter: 'title'
     }
   },
   methods: {
     onInput(event) {
-      this.$emit("termChange", event.target.value, this.searchFilter)
+      this.searchTerm = event.target.value
+      this.$emit("termChange", this.searchTerm, this.searchFilter)
     },
     onRadioClick(event) {
       this.searchFilter = event.target.value
+      this.$emit("termChange", this.searchTerm, this.searchFilter)
     }
   }
 };
